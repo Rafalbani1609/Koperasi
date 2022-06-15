926d22d72010e4bcddce1d9cd57514dd4fb2ee17
Rafalabni1609 <rafalbani12345@egmail.com>
2022-06-15 19:55:47 +0700

    halamanlogin_&_cardbox

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,111 +1,69 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        @php
-            $logo=asset(Storage::url('logo/'));
-        @endphp
-        <meta charset="utf-8" />
-        <title>Sistem Informasi Koperasi Karyawan</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-        <meta content="Aplikasi Koperasi Untuk Memenuhi Kebutuhan Administrasi Di Dalam Koperasi." name="description" />
-        <meta content="Asep IT" name="author" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
-        <!-- App favicon -->
-        <link rel="shortcut icon" class="img-fluid logo-img" href="{{ $logo.'/logo.png' }}">
-        <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        <!-- App css -->
-        <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
-        <link href="{{ asset('css/icons.css') }}" rel="stylesheet" type="text/css" />
-        <link href="{{ asset('css/metismenu.min.css') }}" rel="stylesheet" type="text/css" />
-        <link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css" />
+<head>
+    <title>Koperasi | Login</title>
 
-        <script src="assets/js/modernizr.min.js"></script>
-        @php
-            $logo=asset(Storage::url('logo/'));
-        @endphp
-    </head>
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="../../assets/css/login.css">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
 
+    <script src="https://kit.fontawesome.com/a81368914c.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <body class="bg-accpunt-pages">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- HOME -->
-        <section>
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-12">
+</head>
 
-                        <div class="wrapper-page">
-
-                            <div class="account-pages">
-                                <div class="account-box">
-                                    <div class="account-logo-box">
-                                        <h2 class="text-uppercase text-center">
-                                            <a href="{{ url('/') }}" class="text-success">
-                                                <span><img class="img-fluid logo-img" src="{{$logo.'/small_logo.png' }}" alt="" width="90"></span>
-                                            </a>
-                                        </h2>
-                                        <h6 class="text-uppercase text-center font-bold mt-4">
-                                            SIGN IN
-                                        </h6>
-                                    </div>
-                                    <div class="account-content">
-                                        <form method="POST" action="{{ route('login') }}" class="form-horizontal">
-                                            @csrf
-                                            <div class="form-group m-b-20 row">
-                                                <div class="col-12">
-                                                    <label for="emailaddress">Email atau No Anggota</label>
-                                                    <input id="email" type="text" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" autocomplete="email" autofocus>
-                                                    @error('email')
-                                                        <span class="invalid-feedback" role="alert">
-                                                            <strong>{{ $message }}</strong>
-                                                        </span>
-                                                    @enderror
-                                                </div>
-                                            </div>
-
-                                            <div class="form-group row m-b-20">
-                                                <div class="col-12">
-                                                    <label for="password">Password</label>
-                                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" autocomplete="current-password">
-                                                    @error('password')
-                                                        <span class="invalid-feedback" role="alert">
-                                                            <strong>{{ $message }}</strong>
-                                                        </span>
-                                                    @enderror
-                                                </div>
-                                            </div>
-                                            <div class="form-group row text-center m-t-10">
-                                                <div class="col-12">
-                                                    <button class="btn btn-block btn-gradient waves-effect waves-light" type="submit">Sign In</button>
-                                                </div>
-                                            </div>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- end card-box-->
-
-
-                        </div>
-                        <!-- end wrapper -->
-
+<body>
+    <img class="wave" src="../../images/ombak4.png">
+    <div class="container">
+        <div class="img">
+            <img src="../../images/koprasi.svg">
+        </div>
+        <div class="login-content">
+            <form method="POST" action="{{ route('login') }}" class="form-horizontal">
+                @csrf
+                <img src="../../images/logo.svg" width="70%">
+                <h2 class="title">Welcome</h2>
+                <div class="input-div one">
+                    <div class="i">
+                        <i class="fas fa-user"></i>
+                    </div>
+                    <div class="div">
+                        <h5>Email atau No Anggota</h5>
+                        <input id="gmail" type="text" class="form-control @error('email') is-invalid @enderror"
+                            name="email" value="{{ old('email') }}" autocomplete="email" autofocus>
+                        @error('email')
+                        <span id="peringatan" class="invalid-feedback" role="alert" style="color:red">
+                            <strong><br><br><br>{{ $message }}</strong>
+                        </span>
+                        @enderror
                     </div>
                 </div>
-            </div>
-        </section>
-        <!-- END HOME -->
-        <!-- jQuery  -->
-        <script src="{{ asset('js/jquery.min.js') }}"></script>
-        <script src="{{ asset('js/popper.min.js') }}"></script>
-        <script src="{{ asset('js/bootstrap.min.js') }}"></script>
-        <script src="{{ asset('js/metisMenu.min.js') }}"></script>
-        <script src="{{ asset('js/waves.js') }}"></script>
-        <script src="{{ asset('js/jquery.slimscroll.js') }}"></script>
+                <div class="input-div pass" style="margin-top:45px">
+                    <div class="i">
+                        <i class="fas fa-lock"></i>
+                    </div>
+                    <div class="div">
+                        <h5>Password</h5>
+                        <input id="sandi" type="password"
+                            class="form-control @error('password') is-invalid @enderror" name="password"
+                            autocomplete="current-password">
+                        @error('password')
+                        <span id="peringatan" class="invalid-feedback" role="alert" style="color:red">
+                            <strong><br><br><br>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+                </div>
+                <br><br>
+                <button class="btn" type="submit">Login</button>
+            </form>
+        </div>
+    </div>
+    <script type="text/javascript" src="../../javascript/main.js"></script>
+</body>
 
-        <!-- App js -->
-        <script src="{{ asset('js/jquery.core.js') }}"></script>
-        <script src="{{ asset('js/jquery.app.js') }}"></script>
-    </body>
 </html>
